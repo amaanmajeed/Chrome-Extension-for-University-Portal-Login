a2afa841fecd76bcaffc35812ec64cb901180a9e
amaanmajeed <amaanmajid123@gmail.com>
2023-02-09 16:51:09 +0500

    First working prototype

--- content.js
+++ content.js
@@ -3,15 +3,8 @@ chrome.runtime.sendMessage({type: "getCredentials"}, function(credentials) {
     document.getElementById("Password").value = credentials.password;
 });
 
-document.getElementById("SecurityCode").value = "";
-document.getElementById("SecurityCode").removeAttribute("autocomplete");
-document.getElementById("SecurityCode").removeAttribute("oncopy");
-document.getElementById("SecurityCode").removeAttribute("ondrag");
-document.getElementById("SecurityCode").removeAttribute("ondrop");
-document.getElementById("SecurityCode").removeAttribute("onpaste");
+var captcha = document.querySelector('.captcha').innerText;
+document.getElementById("SecurityCode").value = captcha;
 
-chrome.runtime.sendMessage({type: "getCaptcha"}, function(captcha) {
-    document.getElementById("SecurityCode").value = captcha;
-});
-
-document.getElementById("form0").submit();
+const submitButton = document.querySelector('input[type="submit"]');
+ submitButton.click();
