f01431ffacb27135c1610be11507b6de7a89ae25
amaanmajeed <amaanmajid123@gmail.com>
2023-02-13 00:41:36 +0500

    ft. Added LMS login aswell

--- content.js
+++ content.js
@@ -1,11 +1,24 @@
-var captcha = document.querySelector('.captcha').innerText;
-document.getElementById("SecurityCode").value = captcha;
+if (document.getElementById("SecurityCode")) {
+  var captcha = document.querySelector('.captcha').innerText;
+  document.getElementById("SecurityCode").value = captcha;
+}
 
-setTimeout(function () {
+var std_id = "username";
+var pass_code = "password";
+
+if (document.getElementById("student_id")) {
+  setTimeout(function () {
     // enter student id and password in the respective fields
-    document.getElementById("student_id").value = "student id";
-    document.getElementById("Password").value = "password";
+    document.getElementById("student_id").value = std_id;
+    document.getElementById("Password").value = pass_code;
 }, 500);
+} else {
+  setTimeout(function () {
+    // enter student id and password in the respective fields
+    document.getElementById("username").value = std_id;
+    document.getElementById("password").value = pass_code;
+}, 500);
+}
 
 setTimeout(function(){
   document.getElementById("loginbtn").click();
